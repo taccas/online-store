f838daab8a8077ddaf8160b0da0e7187338be888
taccas <70452140+taccas@users.noreply.github.com>
2022-12-18 23:21:26 +0100

    Update productss.php

--- php/productss.php
+++ php/productss.php
@@ -1,16 +1,16 @@
 <?php
 
-$conn = mysqli_connect("localhost", "superadmin", "fisher1448", "online-store");
+    $conn = mysqli_connect("localhost", "superadmin", "fisher1448", "online-store");
 
-// request for all products
-$sql = "SELECT * FROM products";
+    // request for all products
+    $sql = "SELECT * FROM products";
 
-// make request and get result
-$result = mysqli_query($conn, $sql);
+    // make request and get result
+    $result = mysqli_query($conn, $sql);
 
-// get 
-$products = mysqli_fatch_all($result, MYSQLI_ASSOC);
+    // get 
+    $products = mysqli_fatch_all($result, MYSQLI_ASSOC);
 
-print_r($products)
+    print_r($products);
 
 ?>
