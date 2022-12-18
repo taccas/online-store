1277a3e3299e28ea29d1048c2da7fd70f3097997
taccas <70452140+taccas@users.noreply.github.com>
2022-12-18 22:30:42 +0100

    Update products.php

--- php/products.php
+++ php/products.php
@@ -1,46 +1,43 @@
+<html>
+<body>
+
+<h2>Product List</h2>
+
 <?php
-// Connect to the database
-$host = 'localhost';
-$user = 'superadmin';
-$password = 'fisher1448';
-$database = 'online-store';
+// Connect to the MySQL database
+$host = "localhost";
+$user = "superadmin";
+$password = "fisher1448";
+$dbname = "online-store";
 
-$conn = mysqli_connect($host, $user, $password, $database);
+$conn = mysqli_connect($host, $user, $password, $dbname);
+
 if (!$conn) {
-  die('Error connecting to database: ' . mysqli_error($conn));
+  die("Connection failed: " . mysqli_connect_error());
 }
 
-// Retrieve the list of products from the database
-$result = mysqli_query($conn, 'SELECT * FROM products');
-if (!$result) {
-  die('Error executing query: ' . mysqli_error($conn));
+// Query the products table
+$sql = "SELECT * FROM products";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // Output the product names and prices, along with a "Order" button for each product
+  while($row = mysqli_fetch_assoc($result)) {
+    $product_name = $row['product_name'];
+    $product_price = $row['price'];
+    echo "$product_name - $product_price <br>";
+    echo "<form action='checkout.php' method='post'>";
+    echo "<input type='hidden' name='product_name' value='$product_name'>";
+    echo "<input type='hidden' name='product_price' value='$product_price'>";
+    echo "<input type='submit' value='Order'>";
+    echo "</form>";
+  }
+} else {
+  echo "No products available";
 }
 
-// Fetch the rows of the result set as an array
-$products = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-// Close the connection to the database
 mysqli_close($conn);
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Products</title>
-</head>
-<body>
-  <h1>All Products</h1>
-
-  <ul>
-    <?php foreach ($products as $product): ?>
-      <li>
-        <?php echo $product['product_name']; ?> - $<?php echo $product['price']; ?> - $<?php echo $product['product_desc']; ?>
-        <form action="checkout.php" method="get">
-          <input type="hidden" name="product_id" value="<?php echo $product['id']; ?>">
-          <input type="submit" value="Buy">
-        </form>
-      </li>
-    <?php endforeach; ?>
-  </ul>
 </body>
 </html>
