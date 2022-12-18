a1f7d8721c8e0659372d3de25ab88ebed4905e78
taccas <70452140+taccas@users.noreply.github.com>
2022-12-18 21:54:35 +0100

    Update contact-us.php

---
+++ php/contact-us.php
@@ -0,0 +1,42 @@
+<?php
+// Connect to the database
+$host = 'localhost';
+$user = 'username';
+$password = 'password';
+$database = 'online-store';
+
+$conn = mysqli_connect($host, $user, $password, $database);
+if (!$conn) {
+  die('Error connecting to database: ' . mysqli_error($conn));
+}
+
+// Retrieve the list of products from the database
+$result = mysqli_query($conn, 'SELECT * FROM products');
+if (!$result) {
+  die('Error executing query: ' . mysqli_error($conn));
+}
+
+// Fetch the rows of the result set as an array
+$products = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// Close the connection to the database
+mysqli_close($conn);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Products</title>
+</head>
+<body>
+  <h1>All Products</h1>
+
+  <ul>
+    <?php foreach ($products as $product): ?>
+      <li>
+        <?php echo $product['name']; ?> - $<?php echo $product['price']; ?>
+        <form action="checkout.php" method="get">
+          <input type="hidden" name="product_id" value="<?php echo $product['id']; ?>">
+          <input type="submit" value="Buy">
+        </form>
+     
