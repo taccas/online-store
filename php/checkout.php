5bb86980600d69958278a5f010e57bf399f45deb
taccas <70452140+taccas@users.noreply.github.com>
2022-12-18 22:23:18 +0100

    Update checkout.php

--- php/checkout.php
+++ php/checkout.php
@@ -1,60 +1,50 @@
-<?php
-// Connect to the database
-$host = 'localhost';
-$user = 'superadmin';
-$password = 'fisher1448';
-$database = 'online-store';
-
-$conn = mysqli_connect($host, $user, $password, $database);
-if (!$conn) {
-  die('Error connecting to database: ' . mysqli_error($conn));
-}
-
-// Check if the product_id query parameter is set
-if (isset($_GET['product_id'])) {
-  // Retrieve the product details from the database
-  $product_id = (int)$_GET['product_id'];
-  $result = mysqli_query($conn, "SELECT * FROM products WHERE id = $product_id");
-  if (!$result) {
-    die('Error executing query: ' . mysqli_error($conn));
-  }
-  $product = mysqli_fetch_assoc($result);
-
-  // Check if the form has been submitted
-  if (isset($_POST['submit'])) {
-    // Sanitize the form data
-    $first_name = mysqli_real_escape_string($conn, $_POST['first_name']);
-    $last_name = mysqli_real_escape_string($conn, $_POST['last_name']);
-    $address = mysqli_real_escape_string($conn, $_POST['address']);
-
-    // Insert the data into the database
-    $query = "INSERT INTO orders (first_name, last_name, address, product_id) VALUES ('$first_name', '$last_name', '$address', $product_id)";
-    $result = mysqli_query($conn, $query);
-    if (!$result) {
-      die('Error executing query: ' . mysqli_error($conn));
-    }
-  }
-}
-?>
-
-<!DOCTYPE html>
 <html>
-<head>
-  <title>Checkout</title>
-</head>
 <body>
-  <h1>Checkout</h1>
 
-  <?php if (isset($product)): ?>
-    <h2><?php echo $product['name']; ?></h2>
-    <p>Price: $<?php echo $product['price']; ?></p>
+<h2>Order Form</h2>
 
-    <form action="checkout.php?product_id=<?php echo $product_id; ?>" method="post">
-  <label for="first_name">First Name:</label><br>
-  <input type="text" id="first_name" name="first_name"><br>
-  <label for="last_name">Last Name:</label><br>
-  <input type="text" id="last_name" name="last_name"><br>
-  <label for="address">Address:</label><br>
-  <input type="text" id="address" name="address"><br><br>
-  <input type="submit" name="submit" value="Place Order">
-</form>
+<form action="process_order.php" method="post">
+  Card Number:<br>
+  <input type="text" name="card_number" value=""><br>
+  Expiration Date:<br>
+  <input type="text" name="expiration_date" value=""><br>
+  CVV:<br>
+  <input type="text" name="cvv" value=""><br>
+  Email:<br>
+  <input type="text" name="email" value=""><br>
+  <br>
+  <?php
+  // Connect to the MySQL database
+  $host = "localhost";
+  $user = "superadmin";
+  $password = "fisher1448";
+  $dbname = "online-store";
+
+  $conn = mysqli_connect($host, $user, $password, $dbname);
+
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+
+  // Query the products table
+  $sql = "SELECT * FROM products";
+  $result = mysqli_query($conn, $sql);
+
+  if (mysqli_num_rows($result) > 0) {
+    // Output the product names as checkboxes
+    while($row = mysqli_fetch_assoc($result)) {
+      $product_name = $row['name'];
+      echo "<input type='checkbox' name='products[]' value='$product_name'> $product_name<br>";
+    }
+  } else {
+    echo "No products available";
+  }
+
+  mysqli_close($conn);
+  ?>
+  <br>
+  <input type="submit" value="Submit">
+</form> 
+
+</body>
+</html>
