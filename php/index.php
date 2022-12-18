8fdb3a24988e71bd11c3f5ad3e228b4fa9419e27
taccas <70452140+yourprofit@users.noreply.github.com>
2022-12-18 20:29:55 +0100

    php test

--- php/index.php
+++ php/index.php
@@ -1,5 +1,55 @@
 <?php
 
-echo "Hello World!";
+// Connect to the MySQL database
+$host = "localhost";
+$user = "username";
+$password = "password";
+$dbname = "online-store";
+
+$conn = mysqli_connect($host, $user, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Query the products table
+$sql = "SELECT * FROM products";
+$result = mysqli_query($conn, $sql);
+
+// Check if the query was successful
+if (mysqli_num_rows($result) > 0) {
+    // Output the products
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "ID: " . $row["id"]. " - Name: " . $row["name"]. " - Price: " . $row["price"]. "<br>";
+    }
+} else {
+    echo "No products found.";
+}
+
+// Close the connection
+mysqli_close($conn);
 
 ?>
+
+<!-- HTML button to trigger the PHP code -->
+<button onclick="viewAll()">View All</button>
+
+<!-- JavaScript function to trigger the PHP code -->
+<script>
+function viewAll() {
+  // Make an AJAX request to the PHP file
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      // Update the page with the product data
+      document.getElementById("products").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "view-all.php", true);
+  xhttp.send();
+}
+</script>
+
+<!-- Div to hold the product data -->
+<div id="products"></div>
