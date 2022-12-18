677ac3c01ce7d3afaaeb55fec0b3ee4fb10f409b
taccas <70452140+yourprofit@users.noreply.github.com>
2022-12-18 20:51:05 +0100

    Update index.php

--- php/index.php
+++ php/index.php
@@ -8,16 +8,16 @@ $dbname = "online-store";
 
 $conn = mysqli_connect($host, $user, $password, $dbname);
 
-// Check connection
+// Check MySQL connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Query the products table
+// Choose what information to request
 $sql = "SELECT * FROM products";
 $result = mysqli_query($conn, $sql);
 
-// Check if the query was successful
+// Check if the request was successful
 if (mysqli_num_rows($result) > 0) {
     // Output the products
     while($row = mysqli_fetch_assoc($result)) {
@@ -32,23 +32,12 @@ mysqli_close($conn);
 
 ?>
 
-<!-- HTML button to trigger the PHP code -->
+<!-- Button to trigger PHP code -->
 <button onclick="viewAll()">View All</button>
 
 <!-- JavaScript function to trigger the PHP code -->
 <script>
 function viewAll() {
-  // Make an AJAX request to the PHP file
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      // Update the page with the product data
-      document.getElementById("products").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "view-all.php", true);
-  xhttp.send();
-}
 </script>
 
 <!-- Div to hold the product data -->
